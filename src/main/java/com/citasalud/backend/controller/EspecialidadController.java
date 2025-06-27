fd56fd1d8da43471ee88534d4fb5e591da9c37d0
Juan Carlos Gómez <juan.gomez77@udea.edu.co>
2025-06-26 22:58:48 -0500

    se retiran las anotaciones CrossOrigin que generaban problemas en Sonar

--- src/main/java/com/citasalud/backend/controller/EspecialidadController.java
+++ src/main/java/com/citasalud/backend/controller/EspecialidadController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/especialidades")
-@CrossOrigin(origins = "*")
 public class EspecialidadController {
 
     private final EspecialidadRepository especialidadRepository;
